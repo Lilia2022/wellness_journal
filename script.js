ed42ba848b2822ac417da49cb5f6148ba66c1c97
Lilia2022 <razafimiadana21411@iiitd.ac.in>
2024-04-14 01:36:51 +0530

    library

--- script.js
+++ script.js
@@ -14,7 +14,23 @@ function togglemenu(){
 // ---initializing calendar
 $(document).ready(function(){
     $('#calendar').evoCalendar({
-        settingName: settingValue
+        calendarEvents: [{
+            id: 'event1' ,  //required
+            name: "New year", //required
+            date: "January/1/2024", //required
+            description: "Wish you happy new year",
+            type: "holiday", //required
+            everyYear: true //optional
+        }, 
+        {
+            name: "Vacation Leave",
+            badge: "02/13 - 02/15", // Event badge (optional)
+            date: ["February/13/2020", "February/15/2020"], // Date range
+            description: "Vacation leave for 3 days.", // Event description (optional)
+            type: "event",
+            color: "#63d867" // Event custom color (optional)
+          }
+    ]
     })
 })
   
