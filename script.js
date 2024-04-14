7648da2ef156ff3c2801bbb4dbd91f7668382236
Lilia2022 <razafimiadana21411@iiitd.ac.in>
2024-04-14 14:05:50 +0530

    journal popup

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//small screen menu
 let menuList = document.getElementById("menuList");
 menuList.style.maxHeight ="0px";
 
@@ -10,32 +11,21 @@ function togglemenu(){
     }
 
 
-}
+};
 
-// Initialize evo-calendar in your script file or an inline <script> tag
-$(document).ready(function() {
-    $('#calendar').evoCalendar({
-        // settingName: settingValue
-        calendarEvents: [
-            {
-              id: 'bHay68s', // Event's ID (required)
-              name: "New Year", // Event name (required)
-              date: "January/1/2020", // Event date (required)
-              type: "holiday", // Event type (required)
-              everyYear: true // Same event every year (optional)
-            },
-            {
-              name: "Vacation Leave",
-              badge: "02/13 - 02/15", // Event badge (optional)
-              date: ["February/13/2020", "February/15/2020"], // Date range
-              description: "Vacation leave for 3 days.", // Event description (optional)
-              type: "event",
-              color: "#63d867" // Event custom color (optional)
-            }
-          ]
-    })
-})
+//journal popup form
+
+const addBox = document.querySelector(".add-box"),
+popupBox = document.querySelector(".popup-box"),
+closeIcon = popupBox.querySelector("header i");
 
 
+addBox.addEventListener("click", ()=>{
+    popupBox.classList.add("show");
+});
+
+closeIcon.addEventListener("click", ()=>{
+  popupBox.classList.remove("show");
+});
 
 
