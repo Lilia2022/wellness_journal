d9d147f7ed8ef4bd64459c519103f018cecfbf2e
Lilia2022 <razafimiadana21411@iiitd.ac.in>
2024-04-14 07:46:23 +0530

    calendar

--- script.js
+++ script.js
@@ -12,29 +12,30 @@ function togglemenu(){
 
 }
 
-// ---initializing calendar
-$(document).ready(function(){
+// Initialize evo-calendar in your script file or an inline <script> tag
+$(document).ready(function() {
     $('#calendar').evoCalendar({
-
-        // theme: "Midnight Blue",
-        calendarEvents: [{
-            id: 'event1' ,  //required
-            name: "New year", //required
-            date: "January/1/2024", //required
-            description: "Wish you happy new year",
-            type: "holiday", //required
-            everyYear: true //optional
-        }, 
-        {
-            name: "Vacation Leave",
-            badge: "02/13 - 02/15", // Event badge (optional)
-            date: ["February/13/2020", "February/15/2020"], // Date range
-            description: "Vacation leave for 3 days.", // Event description (optional)
-            type: "event",
-            color: "#63d867" // Event custom color (optional)
-          }
-    ]
+        // settingName: settingValue
+        calendarEvents: [
+            {
+              id: 'bHay68s', // Event's ID (required)
+              name: "New Year", // Event name (required)
+              date: "January/1/2020", // Event date (required)
+              type: "holiday", // Event type (required)
+              everyYear: true // Same event every year (optional)
+            },
+            {
+              name: "Vacation Leave",
+              badge: "02/13 - 02/15", // Event badge (optional)
+              date: ["February/13/2020", "February/15/2020"], // Date range
+              description: "Vacation leave for 3 days.", // Event description (optional)
+              type: "event",
+              color: "#63d867" // Event custom color (optional)
+            }
+          ]
     })
 })
-  
+
+
+
 
